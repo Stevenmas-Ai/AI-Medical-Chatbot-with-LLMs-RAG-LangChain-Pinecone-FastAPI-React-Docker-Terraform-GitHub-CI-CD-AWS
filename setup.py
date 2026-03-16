7017bad87ff37920b573db22558ad1a230593941
Stevenmas23 <stevenmas23@gmail.com>
2026-03-16 11:35:40 +0000

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="steven gerard mascarenhas",
+    author_email="stevengmascarenhas@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+
+)
